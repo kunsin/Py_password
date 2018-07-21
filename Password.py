b11822c0a36c7d1caaa5e219c3581b99d9a7cf9f
Kunsin Lin <kunsinlin@gmail.com>
2018-07-21 20:34:37 +0800

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,9 +1,13 @@
 # Password 小程式
 
-password = 'a123456' 								#建立密碼
-tryit = 2											#錯了以後還可以輸入兩次
+#建立密碼
+password = 'a123456'
 
-while True:											#迴圈區塊 START
+#錯了以後還可以輸入兩次
+tryit = 2
+
+#迴圈區塊 START
+while True:											
 	ask = input('請輸入密碼: ')
 
 	if ask == password:
@@ -15,4 +19,5 @@ while True:											#迴圈區塊 START
 	else:
 		print ('密碼輸入錯誤, 還有',tryit,'機會')
 		tryit = tryit - 1
-													#迴圈區塊 END
+#迴圈區塊 END
+													
